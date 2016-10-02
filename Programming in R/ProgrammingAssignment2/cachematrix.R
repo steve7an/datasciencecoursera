906ebfd8c528fd43c77b99b1fc6eeef8462181ba
steve7an <steve.tan.chee.wei@gmail.com>
2016-10-02 16:01:44 +0800

    final version

--- Programming in R/ProgrammingAssignment2/cachematrix.R
+++ Programming in R/ProgrammingAssignment2/cachematrix.R
@@ -1,30 +1,6 @@
 ## Assignment 2: Caching the Inverse of a Matrix
-
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
+## more info can be found about this here: 
+## https://github.com/lgreski/datasciencectacontent/blob/master/markdown/rprog-breakingDownMakeVector.md
 
 ## creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
